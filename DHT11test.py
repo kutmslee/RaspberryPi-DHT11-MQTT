d84e95b28e08ec464fcaf54d903fd7c0cef7c23a
이모세 <38107813+kutmslee@users.noreply.github.com>
2023-06-20 14:18:09 +0900

    Update DHT11test.py

--- DHT11test.py
+++ DHT11test.py
@@ -3,7 +3,7 @@ import paho.mqtt.client as mqtt
 import time
 
 client=mqtt.Client()
-client.connect('mqtt-dashboard.com', 1883)
+client.connect('mqtt brocker URL', 1883)
 client.loop_start()
 
 while True:
@@ -11,5 +11,5 @@ while True:
 
     data = time.strftime('%y-%m-%d %H:%M:%S  ') + str(temperature) + '도'
     print(data)
-    client.publish('lms/tmp', data, 1)
+    client.publish('topic/tmp', data, 1)
     time.sleep(1h)
